6db2a04106f1414715b1a82f56e37a6d64b62adb
serhanelmali <serhanelmali@gmail.com>
2021-06-08 23:42:20 +0300

    feat(dropdown): categories listed

--- src/components/shared-components/DropDown/index.js
+++ src/components/shared-components/DropDown/index.js
@@ -1,19 +1,75 @@
-import React from "react";
+import React, { useState } from "react";
 import { useAppContext } from "../../../context/state";
 import "./dropdown.scss";
 
 const DropDown = () => {
   const state = useAppContext();
+  const [services] = useState([
+    "Accesories",
+    "Alcohol",
+    "Art",
+    "Books",
+    "Drink",
+    "Electronics",
+    "Flowers & Plants",
+    "Food",
+    "Gadgets",
+    "Garden",
+    "Grocery",
+    "Home",
+    "Jewelry",
+    "Kids & Baby",
+    "Men's Fashion",
+    "Mobile",
+    "Motorcycles",
+    "Movies",
+    "Music",
+    "Office",
+    "Pets",
+    "Romantic",
+    "Sport",
+    "Toys",
+  ]);
 
   return (
-    <>
-      <ul className={state.isDropDown ? "dropdown--active" : "dropdown--off"}>
-        <ul className="categorie__production">
-          <lh>PRODUCTION CATEGORIES</lh>
+    <ul className={state.isDropDown ? "dropdown--active" : "dropdown--off"}>
+      <div className="dropdown__list">
+        <ul className="dropdown__list--header">
+          <lh className="dropdown__list--header--item">PRODUCT CATEGORIE</lh>
+          <lh className="dropdown__list--header--item">SALE</lh>
         </ul>
-        <ul className="categorie__sale"></ul>
-      </ul>
-    </>
+        <div className="dropdown__list--sub">
+          <ul className="dropdown__list--sub__item">
+            {services.slice(0, 8).map((service, index) => (
+              <li className="dropdown__list--sub__item--item" key={index}>
+                {service}
+              </li>
+            ))}
+          </ul>
+          <ul className="dropdown__list--sub__item">
+            {services.slice(8, 16).map((service, index) => (
+              <li className="dropdown__list--sub__item--item" key={index}>
+                {service}
+              </li>
+            ))}
+          </ul>
+          <ul className="dropdown__list--sub__item">
+            {services.slice(16, 24).map((service, index) => (
+              <li className="dropdown__list--sub__item--item" key={index}>
+                {service}
+              </li>
+            ))}
+          </ul>
+          <ul className="dropdown__list--sub__item">
+            {services.slice(0, 8).map((service, index) => (
+              <li className="dropdown__list--sub__item--item" key={index}>
+                {service}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </ul>
   );
 };
 
