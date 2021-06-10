1ce250e98d9fb3cc816607253ce9f08560b49b2b
serhanelmali <serhanelmali@gmail.com>
2021-06-10 11:13:16 +0300

    feat(navigation):dropdown component delete, recreate dropdown in navigation

--- src/components/layouts/Header/index.js
+++ src/components/layouts/Header/index.js
@@ -32,7 +32,7 @@ const Header = () => {
           </li>
         </ul>
       </header>
-      <DropDown />
+      {/* <DropDown /> */}
     </>
   );
 };
--- src/components/shared-components/Navigation/index.js
+++ src/components/shared-components/Navigation/index.js
@@ -1,9 +1,48 @@
 import { useAppContext } from "../../../context/state";
+import Image from "../../../assets/images/dropdownitem.png";
 
 import "./navigation.scss";
 
 const Navigation = () => {
   const state = useAppContext();
+  const services = {
+    ProductCategories: [
+      "Accesories",
+      "Alcohol",
+      "Art",
+      "Books",
+      "Drink",
+      "Electronics",
+      "Flowers & Plants",
+      "Food",
+      "Gadgets",
+      "Garden",
+      "Grocery",
+      "Home",
+      "Jewelry",
+      "Kids & Baby",
+      "Men's Fashion",
+      "Mobile",
+      "Motorcycles",
+      "Movies",
+      "Music",
+      "Office",
+      "Pets",
+      "Romantic",
+      "Sport",
+      "Toys",
+    ],
+    Sale: [
+      "Accesories",
+      "Alcohol",
+      "Art",
+      "Books",
+      "Drink",
+      "Electronics",
+      "Flowers & Plants",
+      "Food",
+    ],
+  };
 
   return (
     <nav className="navigation">
@@ -13,6 +52,33 @@ const Navigation = () => {
           onClick={() => state.onClickHandler()}
         >
           SERVICES
+          <div className="navigation__list--item__services">
+            <ul className="navigation__list--item__services__container">
+              <li className="navigation__list--item__services__container--categorie">
+                <lh className="navigation__list--item__services__container--categorie--title">
+                  Product Categories
+                </lh>
+                <ul className="navigation__list--item__services__container--categorie__list">
+                  {services.ProductCategories.map((product, index) => (
+                    <li>{product}</li>
+                  ))}
+                </ul>
+              </li>
+              <li className="navigation__list--item__services__container--categorie">
+                <lh className="navigation__list--item__services__container--categorie--title">
+                  Sale
+                </lh>
+                <ul className="navigation__list--item__services__container--categorie__list">
+                  {services.Sale.map((sale, index) => (
+                    <li>{sale}</li>
+                  ))}
+                </ul>
+              </li>
+            </ul>
+            <div>
+              <img src={Image} />
+            </div>
+          </div>
         </li>
         <li className="navigation__list--item">COMPANY</li>
         <li className="navigation__list--item">LIBRARY</li>
